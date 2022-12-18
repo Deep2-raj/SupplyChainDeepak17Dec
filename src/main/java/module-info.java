13c9b1289e0494a36fd4c57ea8a347c4ff98f673
deep2-raj <9999deepakverma@gmail.com>
2022-12-18 10:21:13 +0530

    First commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.example.supplychaindeepak17dec {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens com.example.supplychaindeepak17dec to javafx.fxml;
+    exports com.example.supplychaindeepak17dec;
+}
